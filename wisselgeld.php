8899a057f328f3952ba200f1c789f3a6206a3ba4
Mo <moh19951@outlook.com>
2019-11-29 12:02:35 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,35 @@
+<?php
+
+$input = ($argv[1]);
+$geldeenheden = array(50, 20, 10, 5, 2, 1);
+$centen = array(50, 20, 10, 5);
+$geld = intval($input);
+$cent = $input - $geld;
+$cent = intval(round($cent * 100));
+$cent = round($cent / 5) * 5;
+euro($geldeenheden, $geld);
+
+function euro($geldeenheden, $geld)
+{
+    foreach ($geldeenheden as $value) {
+        $restgeld = floor($geld / $value);
+        if ($restgeld >= 1) {
+            $geld = $geld - ($value * $restgeld);
+            echo $restgeld . " x " . $value . " euro" . PHP_EOL;
+        }
+    }
+}
+
+centen($centen, $cent);
+
+function centen($centen, $cent)
+{
+    foreach ($centen as $value) {
+        $restcent = floor($cent / $value);
+        if ($restcent >= 1) {
+            $cent = $cent - ($value * $restcent);
+            echo $restcent . " x " . $value . " cent" . PHP_EOL;
+        }
+    }
+}
+
